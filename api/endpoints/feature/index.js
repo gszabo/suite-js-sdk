08c3010817c4a6dc83a2e86a62c2353500b233eb
m4w4q7 <m4w4q7@gmail.com>
2015-07-13 11:27:26 +0200

    add feature api endpoint

---
+++ api/endpoints/feature/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var logger = require('logentries-logformat')('suite-sdk');
+
+
+var Feature = function(request) {
+  this._request = request;
+};
+
+Feature.prototype = {
+
+  isEnabled: function(customerId, featureId) {
+    logger.log('feature_isOn');
+    return this._request.get('/customers/' + customerId + '/features/' + featureId)
+      .then(function(response) {
+        return response.feature.is_enabled;
+      }.bind(this))
+      .catch(function(error) {
+        logger.log('feature-error', { error: error, stack: error.stack });
+        return false;
+      });
+  }
+
+};
+
+Feature.create = function(request) {
+  return new Feature(request);
+};
+
+module.exports = Feature;
---
+++ api/endpoints/feature/index.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var FeatureAPI = require('./index');
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+describe('Suite Service Feature', function() {
+  var request;
+  var feature;
+
+  describe('Feature enabled', function() {
+
+    it('should check feature status with a Suite api call', function* () {
+      requestPromiseRespondWith({ feature: { is_enabled: true } });
+      var returnValue = yield feature.isEnabled(1, 'test');
+
+      expect(request.get).to.have.been.calledWithMatch('/customers/1/features/test');
+      expect(returnValue).to.eql(true);
+    });
+
+  });
+
+
+  describe('Feature not enabled', function() {
+
+    it('should check feature status with a Suite api call', function* () {
+      requestPromiseRespondWith({ feature: { is_enabled: false } });
+      var returnValue = yield feature.isEnabled(2, 'test');
+
+      expect(request.get).to.have.been.calledWithMatch('/customers/2/features/test');
+      expect(returnValue).to.eql(false);
+    });
+
+  });
+
+
+  it('should log errors and return false', function* () {
+    requestPromiseRejectWith(new Error('404'));
+    var returnValue = yield feature.isEnabled(1, 'test');
+
+    expect(request.get).to.have.been.calledWithMatch('/customers/1/features/test');
+    expect(returnValue).to.eql(false);
+  });
+
+
+  var requestPromiseRespondWith = function(respObj) {
+    var respPromise = new Promise(function(resolve) {
+      resolve(respObj);
+    });
+
+    request = {
+      get: sinon.stub().returns(respPromise),
+      post: sinon.stub().returns(respPromise)
+    };
+    feature = new FeatureAPI(request);
+  };
+
+
+  var requestPromiseRejectWith = function(respObj) {
+    var respPromise = new Promise(function(_, reject) {
+      reject(respObj);
+    });
+
+    request = {
+      get: sinon.stub().returns(respPromise)
+    };
+    feature = new FeatureAPI(request);
+  };
+
+});
